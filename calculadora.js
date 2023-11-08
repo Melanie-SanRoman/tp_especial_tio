6c27d61dfa75e0dffbcd4fd98ddd89ccca302b39
Micaela Lopez <mikalpz04@gmail.com>
2023-11-08 08:49:25 -0300

    Creada funcion calcular con metodo sumar

--- calculadora.js
+++ calculadora.js
@@ -2,3 +2,27 @@ function Limpiar(id1, id2){
     document.getElementById(id1).value = "";
     document.getElementById(id2).value = "";
 }
+function Calcular(id1, id2){
+    var operador = document.getElementById("operadores").options[document.getElementById("operadores").selectedIndex].value;
+    
+    let num1 = document.getElementById(id1).value;
+    let num2 = document.getElementById(id2).value;
+    let resultado = new Number();
+    
+
+    switch(operador)
+    {
+        case "+":
+            resultado= num1 + num2
+            break;
+        case "-":
+        case "/":
+        case "*":
+            					
+        default:
+            break;				
+        
+    }	
+    
+    document.getElementById("salida").innerHTML = "Resultado: "+ resultado;
+}
